9fa8e6ca083420594179db75e078f6cba912c8c9
peilun <peilun2016@gmail.com>
2022-04-03 15:19:45 +0800

    fixs

--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pillow/route_config.dart';
@@ -13,58 +12,102 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double screenSizeWidth = Get.width;
-    double screenSizeHeight = Get.height;
     return SafeArea(
-      child: Scaffold(body: Stack(
+        child: Scaffold(
+      body: Column(
         children: [
-          Row(
+          Expanded(flex: 4, child: top()),
+          Expanded(flex: 4, child: bottom())
+        ],
+      ),
+    ));
+  }
+}
+
+Widget top() {
+  return LayoutBuilder(builder: (context, BoxConstraints constraints) {
+    final w = constraints.maxWidth;
+    final h = constraints.maxHeight;
+    return Stack(
+      children: [
+        Padding(
+          padding: EdgeInsets.fromLTRB(0, w * 0.15, 0, 0),
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Image.asset('assets/head.png', width: screenSizeWidth),
+              Image.asset('assets/head.png', width: w),
             ],
           ),
-          Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: screenSizeHeight*0.09, horizontal: screenSizeWidth*0.1),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Image.asset('assets/pillow.png', width: screenSizeWidth*0.3, height: screenSizeWidth*0.3,)
-                  ],
-                ),
+        ),
+        Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, w * 0.05, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Image.asset(
+                    'assets/pillow.png',
+                    width: w * 0.3,
+                    height: w * 0.3,
+                  )
+                ],
               ),
-              Padding(
-                padding: EdgeInsets.all(30.0),
-                child: Row(
-                  children: [
-                    Image.asset('assets/pillow.png', width: screenSizeWidth*0.3, height: screenSizeWidth*0.3,)
-                  ],
-                ),
-              ),
-            ],
-          ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ElevatedButton(onPressed: (){
-                  Get.toNamed(RouteConfig.rules);
-                }, child: Text('玩法說明')),
-                ElevatedButton(onPressed: (){
-                  Get.toNamed(RouteConfig.bus);
-                }, child: Text('公車時刻')),
-                ElevatedButton(onPressed: (){
-                  Get.toNamed(RouteConfig.lobby);
-                }, child: Text('開始遊戲'))
-              ],
             ),
-          ),
-        ],
-      )),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, w * 0.1, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Image.asset(
+                    'assets/pillow.png',
+                    width: h * 0.3,
+                    height: h * 0.3,
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
     );
-  }
+  });
+}
+
+Widget bottom() {
+  return LayoutBuilder(builder: (context, BoxConstraints constraints) {
+    final w = constraints.maxWidth;
+    final h = constraints.maxHeight;
+    return Stack(children: [
+      Image.asset(
+        'assets/under.png',
+        width: w,
+        height: h * 0.9,
+        fit: BoxFit.cover,
+      ),
+      Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  Get.toNamed(RouteConfig.rules);
+                },
+                child: Text('玩法說明')),
+            ElevatedButton(
+                onPressed: () {
+                  Get.toNamed(RouteConfig.bus);
+                },
+                child: Text('公車時刻')),
+            ElevatedButton(
+                onPressed: () {
+                  Get.toNamed(RouteConfig.lobby);
+                },
+                child: Text('開始遊戲'))
+          ],
+        ),
+      ),
+    ]);
+  });
 }
