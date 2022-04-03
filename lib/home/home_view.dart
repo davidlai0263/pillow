5234344604553af84fc2cbb4d0e3a584b98e58d4
David Lai <spes050111@gmail.com>
2022-04-03 23:27:27 +0800

    Rules completed

--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -15,97 +15,109 @@ class HomePage extends StatelessWidget {
     return SafeArea(
         child: Scaffold(
       body: Column(
-        children: [
-          Expanded(flex: 4, child: top()),
-          Expanded(flex: 5, child: bottom())
+        children: const [
+          Expanded(flex: 4, child: Top()),
+          Expanded(flex: 5, child: Bottom())
         ],
       ),
     ));
   }
 }
 
-Widget top() {
-  return LayoutBuilder(builder: (context, BoxConstraints constraints) {
-    final w = constraints.maxWidth;
-    final h = constraints.maxHeight;
-    return Stack(
-      children: [
-        Padding(
-          padding: EdgeInsets.fromLTRB(0, w * 0.15, 0, 0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
+class Top extends StatelessWidget {
+  const Top({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, BoxConstraints constraints) {
+      final w = constraints.maxWidth;
+      final h = constraints.maxHeight;
+      return Stack(
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, w * 0.15, 0, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset('assets/head.png', width: w),
+              ],
+            ),
+          ),
+          Column(
             children: [
-              Image.asset('assets/head.png', width: w),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, w * 0.05, 0, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Image.asset(
+                      'assets/pillow.png',
+                      width: w * 0.3,
+                      height: w * 0.3,
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, w * 0.1, 0, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.asset(
+                      'assets/pillow.png',
+                      width: h * 0.3,
+                      height: h * 0.3,
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
-        ),
-        Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, w * 0.05, 0, 0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Image.asset(
-                    'assets/pillow.png',
-                    width: w * 0.3,
-                    height: w * 0.3,
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, w * 0.1, 0, 0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Image.asset(
-                    'assets/pillow.png',
-                    width: h * 0.3,
-                    height: h * 0.3,
-                  )
-                ],
-              ),
-            ),
-          ],
-        ),
-      ],
-    );
-  });
+        ],
+      );
+    });
+  }
 }
 
-Widget bottom() {
-  return LayoutBuilder(builder: (context, BoxConstraints constraints) {
-    final w = constraints.maxWidth;
-    final h = constraints.maxHeight;
-    return Stack(alignment: Alignment.bottomCenter, children: [
-      Image.asset(
-        'assets/under.png',
-        width: w,
-        height: h * 0.9,
-        fit: BoxFit.cover,
-      ),
-      Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          ElevatedButton(
+
+class Bottom extends StatelessWidget {
+  const Bottom({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, BoxConstraints constraints) {
+      final w = constraints.maxWidth;
+      final h = constraints.maxHeight;
+      return Stack(alignment: Alignment.bottomCenter, children: [
+        Image.asset(
+          'assets/under.png',
+          width: w,
+          height: h * 0.9,
+          fit: BoxFit.cover,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            ElevatedButton(
               onPressed: () {
                 Get.toNamed(RouteConfig.rules);
               },
-              child: Text('玩法說明')),
-          ElevatedButton(
-              onPressed: () {
-                Get.toNamed(RouteConfig.bus);
-              },
-              child: Text('公車時刻')),
-          ElevatedButton(
-              onPressed: () {
-                Get.toNamed(RouteConfig.lobby);
-              },
-              child: Text('開始遊戲'))
-        ],
-      ),
-    ]);
-  });
+              child: const Text('玩法說明'),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Get.toNamed(RouteConfig.bus);
+                },
+                child: const Text('公車時刻')),
+            ElevatedButton(
+                onPressed: () {
+                  Get.toNamed(RouteConfig.lobby);
+                },
+                child: const Text('開始遊戲'))
+          ],
+        ),
+      ]);
+    });
+  }
 }
--- lib/rules/rules_view.dart
+++ lib/rules/rules_view.dart
@@ -5,12 +5,89 @@ import 'rules_logic.dart';
 
 class RulesPage extends StatelessWidget {
   final logic = Get.put(RulesLogic());
-  final state = Get.find<RulesLogic>().state;
+  final state = Get
+      .find<RulesLogic>()
+      .state;
 
   RulesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final w = Get.width;
+    final h = Get.height;
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Image.asset(
+              'assets/background.png',
+              width: w,
+              fit: BoxFit.cover,
+            ),
+            Column(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(15.0, 10.0, 10.0, 0.0),
+                    child: Stack(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              'assets/how.png',
+                              width: w * 0.3,
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            TextButton(
+                                onPressed: () {
+                                  Get.offAllNamed('/home');
+                                },
+                                child: Image.asset(
+                                  'assets/back.png',
+                                  height: h*0.07,
+                                  width: w * 0.2,
+                                  fit: BoxFit.fill,
+                                )),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                    flex: 5,
+                    child: Image.asset(
+                      'assets/map.png',
+                      width : w,
+                      fit: BoxFit.contain,
+                    )),
+                Expanded(
+                    flex: 6,
+                    child: Center(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                          color: Colors.yellow.shade300.withOpacity(0.6),
+                        ),
+                        width: w * 0.8,
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: w*0.05, vertical: w*0.025),
+                          child: const Text(
+                            '本程式中有提供地圖可以查看地點，到指定地點後會自動跳出挑戰及提示，依照提示指示回答問題若正確的話可以獲得轉盤機會隨機獲得積分，積分可累積並於地圖頁面點選兌換，可依照不同的積分兌換優惠或獎品。',
+                            style: TextStyle(fontSize: 20,),textAlign: TextAlign.center,),
+                        ),
+                      ),
+                    ))
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
