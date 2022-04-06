f60e9c67ee50661907eb2fd08b9e43cc73022eb4
peilun <peilun2016@gmail.com>
2022-04-06 16:53:08 +0800

    add gps listen

--- lib/lobby/lobby_logic.dart
+++ lib/lobby/lobby_logic.dart
@@ -1,7 +1,32 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 
 import 'lobby_state.dart';
 
 class LobbyLogic extends GetxController {
   final LobbyState state = LobbyState();
+  final LocationSettings locationSettings = const LocationSettings(
+    accuracy: LocationAccuracy.high,
+    distanceFilter: 100,
+  );
+
+  late StreamSubscription<Position> positionStream;
+  late LocationPermission permission;
+
+  @override
+  Future<void> onInit() async {
+    permission = await Geolocator.requestPermission();
+    positionStream =
+        Geolocator.getPositionStream(locationSettings: locationSettings)
+            .listen((Position? position) {
+      debugPrint(position == null
+          ? 'Unknown'
+          : '${position.latitude.toString()}, ${position.longitude.toString()}');
+    });
+
+    super.onInit();
+  }
 }
