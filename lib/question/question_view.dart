74cee9a9979150fdc3e10f5588aff7fed571a366
IdONTKnowCHEK <86880683+IdONTKnowCHEK@users.noreply.github.com>
2022-04-08 20:29:33 +0800

    snackBar

--- lib/question/question_view.dart
+++ lib/question/question_view.dart
@@ -1,16 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:pillow/component/doodle_btn/btn_view.dart';
+import 'package:pillow/lobby/lobby_logic.dart';
 
+import '../route_config.dart';
 import 'question_logic.dart';
 
 class QuestionPage extends StatelessWidget {
   final logic = Get.put(QuestionLogic());
   final state = Get.find<QuestionLogic>().state;
-
+  final logicLobby = Get.put(LobbyLogic());
+  final lobbyState = Get.find<LobbyLogic>().state;
   QuestionPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Image.asset(
+            'assets/images/background.png',
+            width: 1.sw,
+            fit: BoxFit.cover,
+          ),
+          Positioned(
+            top: 57.5.h,
+            left: 10.w,
+            child: DoodleBtnWidget(
+              tag: '返回',
+              onTapUpCallback: () {
+                logicLobby.controller.repeat(reverse: true);
+                logicLobby.positionStream.resume();
+                Get.back();
+                // Get.put(LobbyLogic());
+              },
+              facWidth: 0.245,
+              facHeight: 0.07,
+              isText: false,
+            ),
+          ),
+          Positioned(
+            bottom: 50.h,
+            right: 110.w,
+            child: DoodleBtnWidget(
+              onTapUpCallback: () {
+              },
+              facWidth: 0.376,
+              facHeight: 0.085,
+              text: '開始遊戲',
+              activation: true,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
